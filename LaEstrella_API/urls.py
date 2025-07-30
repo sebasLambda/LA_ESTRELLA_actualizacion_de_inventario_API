e544ef177fc41a09d1e5ca8032a929efcf0265a5
Tu Nombre <sebastian.vanegas@lambdaanalytics.co>
2025-07-29 20:19:29 -0500

    first commit

--- LaEstrella_API/urls.py
+++ LaEstrella_API/urls.py
@@ -20,6 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
-    path('api/inmuebles/', include('Inmuebles.urls')),
     path('api/automations/', include('Automations.urls')),
 ]
